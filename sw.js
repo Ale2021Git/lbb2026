b29dc213866d36925aa5bb01df0c297bd82cacb4
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-03-19 21:52:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nome do cache - Incremente a versão sempre que fizer mudanças grandes
-const CACHE_NAME = 'braun-v2.3';
+const CACHE_NAME = 'braun-v2.4';
 
 // Lista de arquivos essenciais (Incluindo os novos screenshots do manifesto)
 const assets = [
